463c79e4b44563aca2dfaf801b4e4c9f1f530e3c
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-15 11:58:55 +0530

    Update validations.js

--- validations.js
+++ validations.js
@@ -1,33 +1,8 @@
 import { Observable } from 'rxjs/Observable';
 
-
-export const CHECK_ID = 'CHECK_ID';
-export const CHECK_INDEX = 'CHECK_INDEX';
 export const APPLY_F = 'APPLY_F';
 export const fx_bar = 'fx_bar';
 
-export function checkIndexAction(id,head,color) {
-  return {
-    type: CHECK_INDEX,
-    payload:{
-        id: id,
-        head: head,
-        color:color
-    } 
-  };
-}
-
-export function checkIDAction(data,head,value,color) {
-  return {
-    type: CHECK_ID,
-    payload:{
-        data: data,
-        head: head,
-        value: value,
-        color:color
-    } 
-  };
-}
 
 export function applyF (fx,l){
   return {
@@ -47,6 +22,7 @@ export function inputEdit(value) {
 }
 
 export function fbar(value) {
+  console.log(value);
   return {
     type: f_bar,
     payload: value 
@@ -56,9 +32,6 @@ export function fbar(value) {
 
 export const inputEditEpic = action$ =>
   action$.ofType(fx_bar)
-    // .subscribe(action =>
-    //   Observable.of(action.payload)
-    //     .subscribe(value => connsole.log(value))
     .mergeMap(action =>
         Observable.of(action.payload)
         .map(value => fbar(value))
