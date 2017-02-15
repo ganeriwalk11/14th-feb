c6deeeb765a41940736561e6283756b659db6a00
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-15 11:57:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import Rx from 'rxjs';
-//import { ajax } from 'rxjs/observable/dom/ajax';
-//import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/dom/ajax';
+import { Observable } from 'rxjs/Observable';
 
 export const FETCH_DATA = 'FETCH_DATA';
 export const FETCH_FUL = 'FETCH_USER_FULFILLED';
 export const FETCH_FULL = 'FETCH_USER_F';
 export const FETCH_STOCK = 'FETCH_STOCK';
 export const ADD_DATA = 'ADD_DATA';
+export const ADD_COL = 'ADD_COL';
 export const CHECK_INTEGER = 'CHECK_INTEGER';
 export const APPLY_FORMULA = 'APPLY_FORMULA';
 export const APPLY_FUN = 'APPLY_FUN';
-import { Observable } from 'rxjs/Observable';
+
 const url = 'src/jsonData/mainData.json';
 
 export const fetchUserEpic = action$ =>
@@ -22,39 +22,7 @@ export const fetchUserEpic = action$ =>
         .map(response => fetchUserFulfilled(response))
     );
 
-  // export const ObjectEpic = action$ =>
-  // action$.ofType(FETCH_FUL)
-  //   .mergeMap(action =>
-  //     ajax.getJSON(url)
-  //       .map(response => fetchUserFulfilled(response))
-  //   );
-
-// export const fetchStockEpic = action$ =>
-//   action$.ofType(FETCH_FUL)
-//     .mergeMap((action) =>
-//             //for(var i=0;i<3;i++){
-//               Observable.ajax.getJSON(action.payload[2]["c"])
-//               .map(response => fetchUserF(response))
-//             //}
-//           //   action.payload.map((row,i) =>
-//           //  Observable.ajax.getJSON(row["c"])
-              
-//           //   )}
-//         // x.subscribe(value => data[1]["c"]["c"] = (value))    //  data.map((row,i) =>{
-//         //  ajax.get(row["c"]["c"])
-//         //  .map(response => response.json()
-//         //  .subscibe(resp => data[i]["c"]["c"] = resp));
-//     );
-    //  console.log(data);
-    //  return fetchUserF(data); 
-    // );
-
-
-
 export const fetchUserFulfilled = payload => ({ type: FETCH_FUL, payload });
-export const fetchUserF = payload => ({ type: FETCH_FULL, payload });
-
-//export const fetchStockValue = a;
 
 export function fetchData() {
   return {
@@ -64,7 +32,7 @@ export function fetchData() {
 
  
 export function postData(data) {
-  console.log(data);
+ // console.log(data);
   //const url = 'src/jsonData/mainData.json';
   const request = axios.post(url,data);
   return ;
@@ -72,8 +40,6 @@ export function postData(data) {
   //   type: FETCH_DATA,
   //   payload: request
   // }
-
- 
 };
 
 export function addData(add) {
@@ -81,6 +47,14 @@ export function addData(add) {
  return {
     type: ADD_DATA,
     payload: addRow
+  };
+};
+
+export function addColData(add) {
+ var addColumn = add;
+ return {
+    type: ADD_COL,
+    payload: addColumn
   };
 };
 
@@ -159,18 +133,20 @@ export function applyFunc(response)
 };
 
 
-export function applyF(a,i,data,color) 
+export function applyF(h,a,i,data,color) 
 {
   var a = a;
   var data = data;
+  var header = h;
+  var color = color;
   let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         var i = i;
         var head = Object.keys(data[0]);
         var len = head.length;
         var op1i = a[3] - 1;
         var op2i = a[6] - 1;
-        var op1j = alpha.indexOf(a[2]) +1;
-        var op2j = alpha.indexOf(a[5]) +1;
+        var op1j = alpha.indexOf(a[2]);
+        var op2j = alpha.indexOf(a[5]);
         var operator = a[4];
         var ans;
 
@@ -179,8 +155,9 @@ export function applyF(a,i,data,color)
         }
         else{
           ans = parseInt(data[op1i][head[op1j]][head[op1j]],10) - parseInt(data[op2i][head[op2j]][head[op2j]],10);
+         // console.log((data[op1i][head[op1j]][head[op1j]],10),parseInt(data[op2i][head[op2j]][head[op2j]],10),ans);
         }
-        var color = color;
+        
         var response = {
           op1i: op1i,
           op1j:op1j,
@@ -191,16 +168,9 @@ export function applyF(a,i,data,color)
           head,head,
           i:i,
           a:a,
-          color:color
+          color:color,
+          header: header
         };
+        console.log(response);
         return {type:APPLY_FUN,payload:response};
-  // return {
-  //   type: APPLY_FORMULA,
-  //   payload: {
-  //     a:a,
-  //     i:i,
-  //     data:data,
-  //     color:color
-  //   }
-  // }
 };
