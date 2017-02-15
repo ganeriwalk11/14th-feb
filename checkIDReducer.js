0dfaa86b9904e7d195e2dcae44e98cd0c61f865c
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-15 11:57:00 +0530

    Update checkIDReducer.js

--- checkIDReducer.js
+++ checkIDReducer.js
@@ -1,38 +1,9 @@
-import { CHECK_ID } from '../actions/validations';
-import { CHECK_INDEX } from '../actions/validations';
 import { fx_bar } from '../actions/validations';
 import { f_bar } from '../actions/validations';
 import { APPLY_F } from '../actions/validations';
 
 export default function(state ="", action) {
   switch (action.type) {
-    case CHECK_INDEX:
-           { switch (action.payload.head["head"]){
-                case "index":
-                    if (action.payload.id == parseInt(action.payload.id, 10))
-                    return {color:'green',borderColor:'3px solid green'};
-                    else
-                    return {color:'red',borderColor:'3px solid red'};
-                    break;
-            default: return {color:'green',borderColor:'3px solid green'};
-            }
-            break;
-        }
-        
-    case CHECK_ID:{
-        var data=[];
-        data = action.payload.data;
-        var head = action.payload.head;
-        var value = action.payload.value;
-        for(var i=0;i<data[0].length;i++)
-        {
-            if(data[0][i][head] == value){
-                return {color:'red',borderColor:'3px solid red'};
-            } 
-        }
-        return {color:'green',borderColor:'3px solid green'};
-        break;
-    }
 
     case APPLY_F: {
         var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -53,13 +24,13 @@ export default function(state ="", action) {
             }
         }
     }
+
     case fx_bar:
         return action.payload;
 
     case f_bar:{
-
-    }
         return state;
+    }
 
     defualt: return state;
     
